b140b3e80ea6f66415dc98a0c906316f5e5e91e0
James Lee <jameslee.98331@gmail.com>
2023-07-05 18:24:14 +0100

    Fix styling and fix deprecated calls

--- mixtures/densities.jl
+++ mixtures/densities.jl
@@ -7,42 +7,47 @@ draw() = (pause(0.001); get_current_fig_manager()[:window][:raise_]())
 
 # Settings
 n = 10000  # sample size to use
-alphas = [Inf,100]  # robustification params to use
-n_samples = 10^4  # number of MCMC iterations
+alphas = [Inf, 100]  # robustification params to use
+n_samples = 10^5  # number of MCMC iterations
 
 # Model parameters and sampler code
 include("setup.jl")
 
-for (i_a,alpha) in enumerate(alphas)
-    x = [(rand()<p1 ? skewrnd(1,l1,s1,a1) : skewrnd(1,l2,s2,a2))[1]::Float64 for j=1:n] # Sample data
+for (i_a, alpha) in enumerate(alphas)
+    # Sample data
+    x = [(rand() < p1 ? skewrnd(1, l1, s1, a1) : skewrnd(1, l2, s2, a2))[1]::Float64 for j = 1:n]
 
     # Run sampler
-    zeta = (1/n)/(1/n + 1/alpha)
-    p,theta,k_r,v_r,art,arv,m_r,s_r = sampler(x,n_samples,m,c,sigma,zeta)
+    zeta = (1 / n) / (1 / n + 1 / alpha)
+    p, theta, k_r, v_r, art, arv, m_r, s_r = sampler(x, n_samples, m, c, sigma, zeta)
 
     # Plot
-    figure(i_a,figsize=(8,3.2)); clf(); hold(true)
+    figure(i_a, figsize=(8, 3.2))
+    clf()
+    # hold(true)
     subplots_adjust(bottom=0.2)
-    xmin,xmax = -5,5
-    xs = linspace(xmin,xmax,1000)
+    xmin, xmax = -5, 5
+    xs = range(xmin, xmax, 1000)
     density = zeros(length(xs))
     for i = 1:m
-        if p[i]>0
-            pixs = p[i]*normpdf(xs,theta[i][1],theta[i][2])
+        if p[i] > 0
+            pixs = p[i] * normpdf.(xs, theta[i][1], theta[i][2])
             density += pixs
-            plot(xs,pixs)
+            plot(xs, pixs)
         end
     end
-    plot(xs,density,"k--",linewidth=3)
-    xlim(xmin,xmax)
-    ylim(0,0.4)
-    if alpha<Inf; title("\$\\mathrm{Coarsened\\,posterior}\$",fontsize=17)
-    else; title("\$\\mathrm{Standard\\,posterior}\$",fontsize=17)
+    plot(xs, density, "k--", linewidth=3)
+    xlim(xmin, xmax)
+    ylim(0, 0.4)
+    if alpha < Inf
+        title("\$\\mathrm{Coarsened\\,posterior}\$", fontsize=17)
+    else
+        title("\$\\mathrm{Standard\\,posterior}\$", fontsize=17)
     end
-    xlabel("\$x\$",fontsize=16)
-    ylabel("\$\\mathrm{density}\$",fontsize=15)
-    draw()
-    savefig("density-alpha=$alpha.png",dpi=150)
+    xlabel("\$x\$", fontsize=16)
+    ylabel("\$\\mathrm{density}\$", fontsize=15)
+    # draw()
+    savefig("density-alpha=$alpha.png", dpi=150)
 end
 
 
